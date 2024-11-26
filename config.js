1ee8be18989c5c975af5e82947733e6298d89097
Monika-Dangar <dangarmonika191023@gmail.com>
2024-11-26 15:36:31 +0530

    Remove config.js from .gitignore

---
+++ config.js
@@ -0,0 +1,5 @@
+export const API_KEY = '8e19a7f7f2bb4187a31603d0f94420b7'
+export const API = "https://api.openweathermap.org/data/2.5"
+export const GEO_API = "http://api.openweathermap.org/geo/1.0/direct"
+export const ICON_URL = "https://openweathermap.org/img/wn/"
+
